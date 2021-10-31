0f12575dce6134adca3a9bb009d17800960e7779
201524547 <qht6@naver.com>
2021-10-31 17:06:35 +0900

    Add Profile Component'

--- src/feature/emotion/screen/MyPageScreen.tsx
+++ src/feature/emotion/screen/MyPageScreen.tsx
@@ -1,15 +1,28 @@
-import {NativeStackScreenProps} from '@react-navigation/native-stack';
-import {tabViewParams} from '@src/feature/tabView/utils/tabview.type';
 import React from 'react';
-import {Text, View} from 'react-native';
+import styled from 'styled-components/native';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+
+import {tabViewParams} from '@src/feature/tabView/utils/tabview.type';
+import Profile from '@src/feature/profile/component/Profile';
 
 type Props = NativeStackScreenProps<tabViewParams, 'MyPage'>;
 
+const Container = styled.View`
+  padding: 20px 0;
+`;
+
 const MyPageScreen = ({}: Props) => {
   return (
-    <View>
-      <Text>마이 페이지 입니다.</Text>
-    </View>
+    <Container>
+      <Profile
+        name={'이준희'}
+        code={'123456'}
+        email={'qht6@naver.com'}
+        photoUrl={
+          'https://firebasestorage.googleapis.com/v0/b/emotion-diary-7b533.appspot.com/o/profile_photo%2FIMG_5846.JPG?alt=media&token=f4db2996-342d-46cf-ae1c-f560dbf31005'
+        }
+      />
+    </Container>
   );
 };
 
---
+++ src/feature/profile/component/Profile.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import styled from 'styled-components/native';
+
+type Props = {
+  name: string;
+  photoUrl: string;
+  code: string;
+  email: string;
+};
+
+const Container = styled.View`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+const ProfilePhoto = styled.Image`
+  width: 100px;
+  height: 100px;
+
+  margin-bottom: 12px;
+
+  border-radius: 50px;
+`;
+
+const Name = styled.Text`
+  margin-bottom: 8px;
+
+  font-size: 20px;
+  font-weight: bold;
+  color: #000;
+`;
+const Code = styled.Text`
+  padding: 2px 4px;
+
+  background-color: #cdcdcd;
+  border: none;
+  border-radius: 4px;
+
+  font-size: 12px;
+  font-weight: normal;
+  color: #080808;
+`;
+
+const Profile = ({name, photoUrl, code}: Props) => {
+  return (
+    <Container>
+      <ProfilePhoto source={{uri: photoUrl}} />
+      <Name>{name}</Name>
+      <Code>{`code : ${code}`}</Code>
+    </Container>
+  );
+};
+
+export default Profile;
