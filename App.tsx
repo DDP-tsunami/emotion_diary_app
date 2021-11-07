8cae0fa5830745744a0c9824eb4e83bed2a482ad
201524547 <qht6@naver.com>
2021-11-07 14:17:39 +0900

    resolve conflict

--- App.tsx
+++ App.tsx
@@ -7,7 +7,7 @@ const theme = {
   space: [0, 4, 8, 12, 16, 20, 24, 32, 40, 48],
   colors: {
     text: '#0A0A0A',
-    background: '#FFF',
+    background: '#5d5dfe',
     border: '#E2E8F0',
     muted: '#F0F1F3',
     success: '#7DBE31',
--- src/common/screen/SplashScreen.tsx
+++ src/common/screen/SplashScreen.tsx
@@ -1,22 +1,46 @@
 /* eslint-disable react-hooks/exhaustive-deps */
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {rootStackParams} from '@src/common/utils/common.types';
 import React, {useEffect} from 'react';
-import {Text, View} from 'react-native';
+import styled from 'styled-components/native';
+import {color} from '../utils/common.style';
 
 type Props = NativeStackScreenProps<rootStackParams, 'Splash'>;
 
+const Container = styled.View`
+  width: 100%;
+
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background-color: ${color.main};
+`;
+
+const Text = styled.Text`
+  color: #fff;
+`;
+
 const SplashScreen = ({navigation}: Props) => {
+  const onSplash = () => {
+    AsyncStorage.getItem('token').then(token => {
+      if (token) {
+        navigation.push('Main');
+      } else {
+        navigation.push('Login');
+      }
+    });
+  };
   useEffect(() => {
-    setTimeout(() => {
-      navigation.push('Login');
-    }, 3000);
+    setTimeout(onSplash, 3000);
   }, []);
 
   return (
-    <View>
+    <Container>
       <Text>Splash 화면 입니다.</Text>
-    </View>
+    </Container>
   );
 };
 
